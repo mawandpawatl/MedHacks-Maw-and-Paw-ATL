bfc011055cf23833d288537f933933a5da123434
LaurenceW <laurence@cosmicarrows.com>
2019-10-27 10:46:26 -0400

    Added Animation for Real-Time Glucose Prototype

--- Maw and Paw ATL/FetchDiningRestaurantsViewController.swift
+++ Maw and Paw ATL/FetchDiningRestaurantsViewController.swift
@@ -18,21 +18,51 @@ class FetchDiningRestaurantsViewController: UIViewController, UITableViewDelegat
     
     var arrayOfCellData = [CellData]()
     
+    var glucoseScore = 120
+    var glucoseActive = true
+    func increaseLabel() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            if self.glucoseScore < 118 {
+                self.currentGlucoseLabel.textColor = UIColor.red
+            } else {
+                self.currentGlucoseLabel.textColor = UIColor.green
+            }
+            if self.glucoseActive {
+                self.glucoseScore -= 1
+                self.currentGlucoseLabel.text = "Current Glucose: \(self.glucoseScore) mg/dL"
+                self.increaseLabel()
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
         arrayOfCellData = [CellData.init(cell: 0, retaurantImage: UIImage.init(named: "carrabba-s-italian-grill-squarelogo"), restaurantName: "Carrabba's", restaurantMainDish: "Most Popular Dish: Le Carte' Peta", restaurantMainDishCalories: "Calories: 27180", glucoseCalculation: "After this meal: 178 mg/dL"), CellData.init(cell: 1, retaurantImage: UIImage.init(named: "millerUnion"), restaurantName: "Miller Union", restaurantMainDish: "Most Popular Dish: Seasonal Vegetable Plate", restaurantMainDishCalories: "Calories: 600", glucoseCalculation: "After this meal: 137 mg/dL"), CellData.init(cell: 2, retaurantImage: UIImage.init(named: "stapleHouse"), restaurantName: "Staple House", restaurantMainDish: "Grandma Lillian’s Potato Bread", restaurantMainDishCalories: "Calories: 1000", glucoseCalculation: "After this meal: 190 mg/dL"), CellData.init(cell: 3, retaurantImage: UIImage.init(named: "kimballHouse"), restaurantName: "Kimball House", restaurantMainDish: "Most Popular Dish: Lamb Loin", restaurantMainDishCalories: "Calories: 1800", glucoseCalculation: "After this meal: 170 mg/dL"), CellData.init(cell: 4, retaurantImage: UIImage.init(named: "tinyLous"), restaurantName: "Tiny Lou's", restaurantMainDish: "Most Popular Dish: Clermont Burger", restaurantMainDishCalories: "Calories: 354", glucoseCalculation: "After this meal: 90 mg/dL"), CellData.init(cell: 5, retaurantImage: UIImage.init(named: "rootBaking"), restaurantName: "Root Baking Co.", restaurantMainDish: "Most Popular Dish: Ham & Cheese Croissant Sandwich", restaurantMainDishCalories: "Calories: 600", glucoseCalculation: "After this meal: 160 mg/dL")]
+        
+        increaseLabel()
+
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return arrayOfCellData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = Bundle.main.loadNibNamed("NewDiningTableViewCell", owner: self, options: nil)?.first as! NewDiningTableViewCell
+        cell.restaurantImage.image = arrayOfCellData[indexPath.row].retaurantImage
+        cell.retaurantCompanyTitle.text = arrayOfCellData[indexPath.row].restaurantName
+        cell.popularDishLabel.text = arrayOfCellData[indexPath.row].restaurantMainDish
+        cell.dishCaloiresLabel.text = arrayOfCellData[indexPath.row].restaurantMainDishCalories
+        cell.realtimeGlucoseLabel.text = arrayOfCellData[indexPath.row].glucoseCalculation
+        return cell
     }
     
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 97
+    }
 
     /*
     // MARK: - Navigation
